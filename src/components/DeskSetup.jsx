5168d2f442bda98fcf635b9421b0d18a93adf8dc
Nicohena <nicohena19@gmail.com>
2026-06-05 18:48:45 +0300

    refactor: optimize 3D scene performance by caching materials for fading and reducing shadow map resolution

--- src/components/DeskSetup.jsx
+++ src/components/DeskSetup.jsx
@@ -5,10 +5,8 @@ export default function DeskSetup() {
   const { scene } = useGLTF('/desk_and_pc.glb')
 
   useEffect(() => {
-    // Log all mesh names so we can identify the system unit
     scene.traverse((child) => {
       if (child.isMesh) {
-        console.log('Mesh:', child.name)
         child.castShadow = true
         child.receiveShadow = true
       }
